a34502ba9fa72860f31ad72c72f18678958fd3e6
Prasad Hari MTTS <prasad-8@mt.com>
2019-02-08 17:48:26 +0530

    Learning promises 08022019

--- Study/Promise.js
+++ Study/Promise.js
@@ -11,6 +11,7 @@
 //     })
 //     .then(() => console.log('I promised to run after 2s'));
 //************************************************************* */
+
 ///////////////////
 //Second example
 ///////////////////
@@ -32,27 +33,63 @@
 //     console.error(err);
 // });
 /**************************************************************** */
+
 ///////////////////
 //Chaining promises
 ///////////////////
-const status = response => {
-    if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response);
-    }
-    return Promise.reject(new Error(response.statusText));
-};
+// const status = response => {
+//     if (response.status >= 200 && response.status < 300) {
+//         return Promise.resolve(response);
+//     }
+//     return Promise.reject(new Error(response.statusText));
+// };
 
-const json = response => response.json();
+// const json = response => response.json();
 
-fetch('todos.json')
-    .then(status)
-    .then(json)
-    .then(data => {
-        console.log('Request succeeded with JSON response', data);
-        var jsonConv = JSON.stringify(data);
-        console.log(jsonConv);
-    })
-    .catch(error => {
-        console.log('Request failed', error);
-    });
+// fetch('JSON/todos.json')
+//     .then(status)
+//     .then(json)
+//     .then(data => {
+//         console.log('Request succeeded with JSON response', data);
+//         var jsonConv = JSON.stringify(data);
+//         console.log(jsonConv);
+//     })
+//     .catch(error => {
+//         console.log('Request failed', error);
+//     });
+/**************************************************************** */
 
+///////////////////
+//Promise.all() - synchronizes multiple promises
+///////////////////
+// const f1 = fetch('JSON/something.json');
+// const f2 = fetch('JSON/something2.json');
+
+// const json = response => response.json();
+
+// Promise.all([f1, f2])
+//     .then(res => {
+//         console.log('Array of results', res);
+
+//         return Promise.resolve(res[0]);
+//     })
+//     .then(json)
+//     .then(data => console.log(data))
+//     .catch(err => {
+//         console.error(err);
+//     });
+/**************************************************************** */
+
+///////////////////
+//Promise.race() - runs as soon as one of the promises resolves first.
+///////////////////
+// const promiseOne = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 500, 'one');
+// });
+// const promiseTwo = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 100, 'two');
+// });
+
+// Promise.race([promiseOne, promiseTwo]).then(result => {
+//     console.log(result); // 'two'
+// });
