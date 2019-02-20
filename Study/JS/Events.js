86ba25346977a765244df2608eb16ae548fc4f18
Prasad Hari MTTS <prasad-8@mt.com>
2019-02-20 18:15:14 +0530

    Study JS 20022019: Events

--- Study/JS/Events.js
+++ Study/JS/Events.js
@@ -12,6 +12,53 @@ window.addEventListener('load', () => {
 //Document events
 /////////////////////////
 const link = document.getElementById("demo");
-link.addEventListener('click', () => {
+link.addEventListener('click', (event) => {
+    prompt("Hello World");
+    event.stopPropagation(); //Stops bubbling or capturing (tunneling)
+});
+
+/////////////////////////
+//Window Events - global events
+/////////////////////////
+window.addEventListener('load', () => {
     prompt("Hello World");
 });
+
+/////////////////////////
+//Throttling - Events like scroll and mousemove will make the website sluggish.
+// Some libraries like LodAsh provides throttling
+/////////////////////////.
+
+//The below code is just for understanding throttling
+let cached = null;
+window.addEventListener('scroll', event => {
+    if (!cached) {
+        setTimeout(() => {
+            //you can access the original event at `cached`
+            cached = null
+        }, 100);
+    }
+    cached = event;
+});
+
+///////////////////////////////
+//Event loops - has a control over the events (Sync/Async)
+// priority of execution
+// 1. Event Loops - all the calls are executed
+// 2. Job queues (ES6)
+// 3. Message Queue - all the event callbacks are executed
+///////////////////////////////
+
+//Promises (Async/Await) will be executed just before the settimeout (old async functions) 
+const bar = () => console.log('bar');
+const baz = () => console.log('baz');
+const foo = () => {
+    console.log('foo');
+    setTimeout(bar, 0);
+    new Promise((resolve, reject) =>
+        resolve('should be right after baz, before bar')
+    ).then(resolve => console.log(resolve));
+    baz();
+}
+
+foo();
